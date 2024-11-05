d4896378a3290b9f916745a4a9beb6e43e3e62ed
Alfie Qashwa <alfieqashwa@gmail.com>
2024-11-05 17:21:46 +0700

    fix: user-info

--- app/(auth)/[slug]/profile/user-info.tsx
+++ app/(auth)/[slug]/profile/user-info.tsx
@@ -1,33 +1,49 @@
 "use client"
 
-import { Building2, LayoutTemplate, MapPin, Phone } from "lucide-react"
-import Image from "next/image"
-import { LoadingSpinner } from "@/app/_components/loading"
+import { LoadingSpinner } from "@/components/loading-spinner"
 import { Separator } from "@/components/ui/separator"
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
-import { env } from "@/env"
-import { api } from "@/trpc/react"
+import { Building2, LayoutTemplate, MapPin, Phone } from "lucide-react"
+import Image from "next/image"
 import { TogglePublished } from "./toggle-published"
 import { UpdateCompanyInfo } from "./update-company-info"
 import { UpdateUserRoleForMeOnly } from "./update-user-role-for-me-only"
+import { convexQuery } from "@convex-dev/react-query"
+import { api } from "@/convex/_generated/api"
+import { useQuery as useTanstackQuery } from "@tanstack/react-query"
+import { Id } from "@/convex/_generated/dataModel"
 
-export function UserInfo({ isAdmin }: { isAdmin: boolean }) {
-  const user = api.user.find.useQuery()
+export function UserInfo({
+  isAdmin,
+  userId,
+  companyId,
+}: {
+  isAdmin: boolean
+  userId: Id<"users">
+  companyId: Id<"companies">
+}) {
+  const { data: userWithCompany, status: StatusUserWithCompany } =
+    useTanstackQuery({
+      enabled: Boolean(userId) && Boolean(companyId),
+      ...convexQuery(api.users.findUserWithCompany, { userId, companyId }),
+    })
 
-  const countAllBooking = api.order.countAllBooking.useQuery(undefined, {
-    refetchInterval: 1000 * 10,
+  const orders = useTanstackQuery({
+    enabled: Boolean(companyId),
+    ...convexQuery(api.orders.findAll, { companyId }),
   })
+
   return (
     <div className="flex flex-col">
-      {user.status === "pending" && <LoadingSpinner />}
-      {user.status === "success" && (
+      {StatusUserWithCompany === "pending" && <LoadingSpinner />}
+      {StatusUserWithCompany === "success" && (
         <div className="px-4">
           <Image
-            src={user.data?.image as string}
+            src={userWithCompany.user?.image as string}
             alt="Profile Image"
             width={500}
             height={500}
@@ -36,24 +52,29 @@ export function UserInfo({ isAdmin }: { isAdmin: boolean }) {
           <section className="mt-6 space-y-2">
             <article>
               <h2 className="text-primary">Email</h2>
-              <p className="text-muted-foreground">{user.data?.email}</p>
+              <p className="text-muted-foreground">
+                {userWithCompany.user?.image}
+              </p>
             </article>
             <article>
               <h2 className="text-primary">Name</h2>
               <p className="capitalize text-muted-foreground">
-                {user.data?.name}
+                {userWithCompany.user?.name}
               </p>
             </article>
             <article>
               <h2 className="text-primary">Role</h2>
               {/* // Only me can access this! */}
-              {user.data?.email === env.NEXT_PUBLIC_DEWA ? (
+              {userWithCompany.user?.email ===
+              process.env.NEXT_PUBLIC_DEWA_EMAIL ? (
                 <UpdateUserRoleForMeOnly
-                  userId={user.data?.id}
-                  userRole={user.data?.role}
+                  userId={userWithCompany.user?._id}
+                  userRole={userWithCompany.user?.role}
                 />
               ) : (
-                <p className="text-muted-foreground">{user.data?.role}</p>
+                <p className="text-muted-foreground">
+                  {userWithCompany.user?.role}
+                </p>
               )}
             </article>
             <Separator />
@@ -62,22 +83,22 @@ export function UserInfo({ isAdmin }: { isAdmin: boolean }) {
             <div className="flex space-x-1">
               <Building2 className="mr-2 shrink-0 text-primary" />
               <p className="space-x-1 capitalize text-muted-foreground">
-                <span>{user.data?.company?.name}</span>
+                <span>{userWithCompany.company?.name}</span>
               </p>
             </div>
             <div className="flex space-x-1">
               <Phone className="mr-2 shrink-0 text-primary" />
               <p className="text-muted-foreground">
-                {user.data?.company?.phone}
+                {userWithCompany.company?.phone}
               </p>
             </div>
             <div className="flex space-x-1">
               <MapPin className="mr-2 shrink-0 text-primary" />
               <p className="space-x-1 text-balance capitalize text-muted-foreground">
-                {user.data?.company?.location}
+                {userWithCompany.company?.location}
               </p>
             </div>
-            {isAdmin && user.data?.company && (
+            {isAdmin && !!userWithCompany.company && (
               <section>
                 <div className="flex space-x-1">
                   <Tooltip>
@@ -94,18 +115,20 @@ export function UserInfo({ isAdmin }: { isAdmin: boolean }) {
                   <p className="text-balance pr-2 capitalize text-muted-foreground">
                     Published?
                   </p>
-                  <TogglePublished
-                    companyId={user.data.company.id}
-                    companyName={user.data.company.name}
-                    isPublished={user.data?.company?.isPublished}
-                    countAllBooking={countAllBooking.data!}
-                  />
+                  {orders.status === "success" && (
+                    <TogglePublished
+                      companyId={userWithCompany.company._id}
+                      companyName={userWithCompany.company.name}
+                      isPublished={userWithCompany.company.isPublished}
+                      countAllBooking={orders.data}
+                    />
+                  )}
                 </div>
                 <UpdateCompanyInfo
                   isAdmin={isAdmin}
-                  companyId={user.data.company.id}
-                  phone={user.data.company.phone}
-                  location={user.data.company.location}
+                  companyId={userWithCompany.company._id}
+                  phone={userWithCompany.company.phone}
+                  location={userWithCompany.company.location}
                   className="mt-4"
                 />
               </section>
