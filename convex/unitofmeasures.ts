a1289c473cc2df262970a5e65b079862893acd2d
Alfie Qashwa <alfieqashwa@gmail.com>
2024-11-07 11:17:35 +0700

    fix: change procedure

--- convex/unitofmeasures.ts
+++ convex/unitofmeasures.ts
@@ -4,12 +4,12 @@ import {
   uomSchema,
 } from "../types/schema/unit-of-measure-schema"
 import { mutation, query } from "./_generated/server"
-import { managerProcedure, protectedProcedure, zMutation } from "./helpers"
+import { superAdminProcedure, zMutation } from "./helpers"
 
 export const findAllByCompanyId = query({
   args: { companyId: v.id("companies") },
   handler: async (ctx) => {
-    await protectedProcedure(ctx, {})
+    await superAdminProcedure(ctx, {})
 
     return await ctx.db.query("unitOfMeasures").collect()
   },
@@ -17,7 +17,7 @@ export const findAllByCompanyId = query({
 export const create = zMutation({
   args: { createUomSchema },
   handler: async (ctx, { createUomSchema: { name, description } }) => {
-    await managerProcedure(ctx, {})
+    await superAdminProcedure(ctx, {})
 
     return await ctx.db.insert("unitOfMeasures", {
       name,
@@ -28,7 +28,7 @@ export const create = zMutation({
 export const update = zMutation({
   args: { uomSchema },
   handler: async (ctx, { uomSchema: { id, name, description } }) => {
-    await managerProcedure(ctx, {})
+    await superAdminProcedure(ctx, {})
 
     return await ctx.db.patch(id, { name, description })
   },
@@ -36,7 +36,7 @@ export const update = zMutation({
 export const remove = mutation({
   args: { id: v.id("unitOfMeasures") },
   handler: async (ctx, args) => {
-    await managerProcedure(ctx, {})
+    await superAdminProcedure(ctx, {})
 
     return await ctx.db.delete(args.id)
   },
