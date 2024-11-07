69466f831a5115fb34d11fce503fcb88182dad25
Alfie Qashwa <alfieqashwa@gmail.com>
2024-11-07 09:11:20 +0700

    feat: config authorization to consume uom api

--- convex/unitofmeasures.ts
+++ convex/unitofmeasures.ts
@@ -1,17 +1,21 @@
 import { v } from "convex/values"
 import { createUomSchema, uomSchema } from "../types/schema/unit-of-measure"
 import { mutation, query } from "./_generated/server"
-import { zMutation } from "./helpers"
+import { managerProcedure, protectedProcedure, zMutation } from "./helpers"
 
 export const findAllByCompanyId = query({
   args: { companyId: v.id("companies") },
-  handler: async (ctx, args) => {
+  handler: async (ctx) => {
+    await protectedProcedure(ctx, {})
+
     return await ctx.db.query("unitOfMeasures").collect()
   },
 })
 export const create = zMutation({
   args: { createUomSchema },
   handler: async (ctx, { createUomSchema: { name, description } }) => {
+    await managerProcedure(ctx, {})
+
     return await ctx.db.insert("unitOfMeasures", {
       name,
       description,
@@ -21,12 +25,16 @@ export const create = zMutation({
 export const update = zMutation({
   args: { uomSchema },
   handler: async (ctx, { uomSchema: { id, name, description } }) => {
+    await managerProcedure(ctx, {})
+
     return await ctx.db.patch(id, { name, description })
   },
 })
 export const remove = mutation({
   args: { id: v.id("unitOfMeasures") },
   handler: async (ctx, args) => {
+    await managerProcedure(ctx, {})
+
     return await ctx.db.delete(args.id)
   },
 })
