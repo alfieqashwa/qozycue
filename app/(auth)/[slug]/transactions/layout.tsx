48c63c496d1c988715026b95f53351c09652d570
Alfie Qashwa <alfieqashwa@gmail.com>
2024-11-11 09:45:28 +0700

    feat: setup parallel-routes on transactions page

--- app/(auth)/[slug]/transactions/layout.tsx
+++ app/(auth)/[slug]/transactions/layout.tsx
@@ -1,14 +1,19 @@
 import { type Metadata } from "next"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { OrderTab } from "./order-tab"
-import { OrderlineTab } from "./orderline-tab"
-import { RentalTab } from "./rental-tab"
 
 export const metadata: Metadata = {
   title: "Transactions",
 }
 
-export default async function TransactionPage() {
+export default async function TransactionLayout({
+  orders,
+  // rentals,
+  // orderlines,
+}: Readonly<{
+  orders: React.ReactNode
+  // rentals: React.ReactNode
+  // orderlines: React.ReactNode
+}>) {
   // ? not allowing owner to click button ???
 
   // const managerAndCashierAccessLevel =
@@ -21,18 +26,15 @@ export default async function TransactionPage() {
     <Tabs defaultValue="order" className="mt-2">
       <TabsList className="mb-3">
         <TabsTrigger value="order">Orders</TabsTrigger>
-        <TabsTrigger value="pool-rental">Pool Rentals</TabsTrigger>
-        <TabsTrigger value="orderline">Orderlines</TabsTrigger>
+        {/* <TabsTrigger value="pool-rental">Pool Rentals</TabsTrigger>
+        <TabsTrigger value="orderline">Orderlines</TabsTrigger> */}
       </TabsList>
       <TabsContent value="order">
-        <OrderTab disabledBasedOnAccessLevel={!managerAndCashierAccessLevel} />
+        {/* <OrderTab disabledBasedOnAccessLevel={!managerAndCashierAccessLevel} /> */}
+        {orders}
       </TabsContent>
-      <TabsContent value="pool-rental">
-        <RentalTab />
-      </TabsContent>
-      <TabsContent value="orderline">
-        <OrderlineTab />
-      </TabsContent>
+      {/* <TabsContent value="pool-rental">{rentals}</TabsContent>
+      <TabsContent value="orderline">{orderlines}</TabsContent> */}
     </Tabs>
   )
 }
