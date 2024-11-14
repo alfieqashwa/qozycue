4b98d2c2e5414d34d9827091adea2e18a690fcf4
Alfie Qashwa <alfieqashwa@gmail.com>
2024-11-14 17:06:14 +0700

    fix: finished setup startTimerButton

--- app/(auth)/[slug]/tables/@poolTables/pool-table-card/start-timer-button.tsx
+++ app/(auth)/[slug]/tables/@poolTables/pool-table-card/start-timer-button.tsx
@@ -28,13 +28,23 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
 import { formattedPrice } from "@/lib/format-price"
 import { cn } from "@/lib/utils"
+import { Rate } from "@/types"
 import { startTimerSchema, type TStartTimer } from "@/types/schema/order-schema"
+import { convexQuery, useConvexMutation } from "@convex-dev/react-query"
 import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  useMutation,
+  useQuery as useTanstackQuery,
+} from "@tanstack/react-query"
+import { ConvexError } from "convex/values"
 import { Loader2, Timer } from "lucide-react"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
+import { toast } from "sonner"
 
 export function StartTimerButton({
   // isCashier,
@@ -43,20 +53,22 @@ export function StartTimerButton({
   gapDuration,
 }: {
   // isCashier: boolean
-  poolTableId: string
+  poolTableId: Id<"poolTables">
   poolTableName: string
   gapDuration: number
 }) {
   const [open, setOpen] = useState(false)
 
-  const countIsBooking = api.poolRental.countIsBooking.useQuery(
-    { poolTableId },
-    { enabled: Boolean(poolTableId) },
-  )
-  const packets = api.packet.findAllByCompanyId.useQuery(undefined, {
+  const countIsBooking = useTanstackQuery({
+    ...convexQuery(api.poolrentals.countIsBooking, { poolTableId }),
+    enabled: Boolean(poolTableId),
+  })
+
+  const packets = useTanstackQuery({
+    ...convexQuery(api.packets.findAll, {}),
     select(data) {
       return data
-        .filter((d) => d.status === Status.enabled)
+        .filter((d) => d.status === "enabled")
         .sort((a, b) => b.cost - a.cost)
         .sort((p, q) =>
           p.rate.localeCompare(q.rate, undefined, { numeric: true }),
@@ -82,34 +94,24 @@ export function StartTimerButton({
   const packetIdWatch = form.watch("packetId")
   const durationWatch = form.watch("duration")
   const isHourly =
-    packets.data?.find((p) => p.id === packetIdWatch)?.rate === "HOUR"
+    packets.data?.find((p) => p._id === packetIdWatch)?.rate === "HOUR"
 
   const handleOnCloseAutoFocus = () => {
     form.reset()
   }
 
-  const { mutate, isPending } = api.poolRental.startTimer.useMutation({
-    async onSuccess() {
-      toast({
-        title: "Succeed!",
-        variant: "default",
-        description: <p>Table {poolTableName} has been started.</p>,
-      })
-      // await utils.poolTable.invalidate()
-      // await utils.order.findByPoolTableId.invalidate({ poolTableId })
-      // await utils.order.findByPoolTableIdPublic.invalidate({ poolTableId })
-      // await utils.poolRental.invalidate()
-      /* auto-closed after succeed submit the dialog form */
-      setOpen(false)
-    },
-    onError(err) {
-      toast({
-        variant: "destructive",
-        title: "Uh oh! Something went wrong.",
-        description: err.message || "There was a problem with your request.",
-        action: <ToastAction altText="Try again">Try again</ToastAction>,
-      })
-    },
+  const { mutate, isPending } = useMutation({
+    mutationFn: useConvexMutation(api.orders.startTimer),
+    onSuccess: () =>
+      toast.success("Succeed!", {
+        description: `Table ${poolTableName} has been started.`,
+      }),
+    onError: (err) =>
+      toast.error("Something went wrong.", {
+        description:
+          err instanceof ConvexError ? err.data : "Unexpected error occurred",
+      }),
+    onSettled: () => setOpen(false),
   })
 
   // 2. Define a submit handler.
@@ -118,21 +120,23 @@ export function StartTimerButton({
 
     if (packets.status !== "success" && packetId === "") return
 
-    const costCalc = packets.data?.find((p) => p.id === packetId)
+    const costCalc = packets.data?.find((p) => p._id === packetId)
       ?.cost as number
-    const rate = packets.data?.find((p) => p.id === packetId)?.rate as Rate
+    const rate = packets.data?.find((p) => p._id === packetId)?.rate as Rate
 
     const durationCalc = rate === "MINUTE" ? 0 : Number(duration)
 
     mutate({
-      gapDuration,
-      customerName: customerName?.toLowerCase(),
-      customerPhone: customerPhone?.trim(),
-      poolTableId,
-      packetId,
-      duration: durationCalc,
-      rate,
-      cost: costCalc,
+      startTimerSchema: {
+        gapDuration,
+        customerName: customerName?.toLowerCase(),
+        customerPhone: customerPhone?.trim(),
+        poolTableId,
+        packetId,
+        duration: durationCalc,
+        rate,
+        cost: costCalc,
+      },
     })
   }
 
@@ -219,9 +223,9 @@ export function StartTimerButton({
                             ?.filter((p) => p.rate === "HOUR")
                             .map((p) => (
                               <SelectItem
-                                value={p.id}
+                                value={p._id}
                                 className="capitalize"
-                                key={p.id}
+                                key={p._id}
                               >
                                 {p.name}
                                 <span className="pl-2 text-xs text-sky-400">
@@ -235,15 +239,15 @@ export function StartTimerButton({
                             ))
                         : packets.data?.map((p) => (
                             <SelectItem
-                              value={p.id}
+                              value={p._id}
                               className="capitalize"
-                              key={p.id}
+                              key={p._id}
                             >
                               {p.name}
                               <span
                                 className={cn(
                                   "pl-2 text-xs",
-                                  p.rate === Rate.HOUR
+                                  p.rate === "HOUR"
                                     ? "text-sky-400"
                                     : "text-amber-300",
                                 )}
