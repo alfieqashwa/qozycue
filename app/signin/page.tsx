30cfa20559c3ba0fab08b7212dd1f8f6fcdfee56
Alfie Qashwa <alfieqashwa@gmail.com>
2024-12-16 22:48:56 +0700

    style: change icon

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,15 +1,16 @@
 import { SignInButton } from "@/components/sign-button"
-import { GitHubLogoIcon } from "@radix-ui/react-icons"
+import { FcGoogle } from "react-icons/fc"
 
 export default function SignInPage() {
   return (
     <div className="container mx-auto my-auto flex min-h-screen w-full">
       <div className="mx-auto my-auto flex max-w-[384px] flex-col gap-4 pb-8">
-        <h2 className="text-2xl font-semibold tracking-tight">
+        <h2 className="whitespace-nowrap text-2xl font-semibold tracking-tight">
           Sign in with Google
         </h2>
         <SignInButton variant="outline" redirectTo="/portal">
-          <GitHubLogoIcon className="mr-2 h-4 w-4" /> Google
+          <FcGoogle size={20} className="mr-2" />
+          <span className="whitespace-nowrap">Sign In</span>
         </SignInButton>
       </div>
     </div>
