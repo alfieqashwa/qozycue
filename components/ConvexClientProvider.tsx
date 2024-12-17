66bc21b8b0ca8a3dfff7f8631c825c903cc56895
Alfie Qashwa <alfieqashwa@gmail.com>
2024-12-17 17:03:39 +0700

    fix: update convex-client-provider based on latest-docs

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -6,9 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ConvexReactClient } from "convex/react"
 import { ReactNode } from "react"
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
-  verbose: true,
-})
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 const convexQueryClient = new ConvexQueryClient(convex)
 const queryClient = new QueryClient({
   defaultOptions: {
