89c4d33126b5866209c960a74444538a94d0b749
Alfie Qashwa <alfieqashwa@gmail.com>
2025-01-12 22:13:02 +0700

    fix(bug): fix bug in auth.ts
    - Problem: whenever super_admin or admin creating new user, name & image are not getting saved in the users table.
    - Solution: added name & image fields in the user object before saving it to the users table.

--- convex/auth.ts
+++ convex/auth.ts
@@ -3,25 +3,42 @@ import { convexAuth } from "@convex-dev/auth/server"
 import { MutationCtx } from "./_generated/server"
 import { findUserByEmail } from "./helpers"
 
+//? Source -> https://labs.convex.dev/auth/advanced#controlling-user-creation-and-account-linking-behavior
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [Google],
   callbacks: {
     // `args.type` is one of "oauth" | "email" | "phone" | "credentials" | "verification"
     // `args.provider` is the currently used provider config
     async createOrUpdateUser(ctx: MutationCtx, args) {
-      console.log(`existingUserId::: `, args.existingUserId)
+      // console.log(`existingUserId::: `, args.existingUserId)
+
       if (args.existingUserId) {
         // Optionally merge updated fields into the existing user object here
+        const existingUser = await findUserByEmail(ctx, args.profile.email)
+
+        if (existingUser) {
+          if (
+            existingUser.name === undefined &&
+            existingUser.image === undefined
+          ) {
+            await ctx.db.patch(existingUser._id, {
+              ...args.profile,
+              name: args.profile.name as string,
+              image: args.profile.image as string,
+            })
+          }
+
+          return existingUser._id
+        }
+
         return args.existingUserId
       }
 
-      const existingUser = await findUserByEmail(ctx, args.profile.email)
-      console.log({ existingUser })
-      if (existingUser) return existingUser._id
+      // console.log({ existingUser })
 
       // Implement your own user creation:
       const isDewa = args.profile.email === process.env.DEWA_EMAIL!
-      return ctx.db.insert("users", {
+      return await ctx.db.insert("users", {
         ...args.profile,
         role: isDewa ? "DEWA" : "USER",
       })
