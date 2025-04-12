ab18bb93e6c15f54ce84c4d42f566d22e01b7db6
Alfie Qashwa <alfieqashwa@gmail.com>
2025-04-12 20:47:22 +0700

    fix: remove optional args PoolTableId

--- convex/poolTables.ts
+++ convex/poolTables.ts
@@ -53,11 +53,10 @@ export const findAll = query({
 })
 
 export const findById = query({
-  args: { poolTableId: v.optional(v.id("poolTables")) },
+  args: { poolTableId: v.id("poolTables") },
   handler: async (ctx, { poolTableId }) => {
     await protectedProcedure(ctx)
 
-    if (!poolTableId) throw new ConvexError("No PoolTable ID provided!")
     return await ctx.db.get(poolTableId)
   },
 })
