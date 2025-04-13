6d55efd968934376435d3127f23cf4ebb337c03e
Alfie Qashwa <alfieqashwa@gmail.com>
2025-04-14 04:20:43 +0700

    fix: wrapped with useMemo to make token rendered once

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -4,27 +4,45 @@ import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs"
 import { ConvexQueryClient } from "@convex-dev/react-query"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ConvexReactClient } from "convex/react"
-import { ReactNode } from "react"
+import { ReactNode, useMemo } from "react"
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
-  verbose: true, // https://labs.convex.dev/auth/debugging
-})
-const convexQueryClient = new ConvexQueryClient(convex)
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      queryKeyHashFn: convexQueryClient.hashFn(),
-      queryFn: convexQueryClient.queryFn(),
-    },
-  },
-})
-convexQueryClient.connect(queryClient)
+// const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
+//   verbose: true, // https://labs.convex.dev/auth/debugging
+// })
+// const convexQueryClient = new ConvexQueryClient(convex)
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       queryKeyHashFn: convexQueryClient.hashFn(),
+//       queryFn: convexQueryClient.queryFn(),
+//     },
+//   },
+// })
+// convexQueryClient.connect(queryClient)
 
 export default function ConvexClientProvider({
   children,
 }: {
   children: ReactNode
 }) {
+  const { convex, queryClient } = useMemo(() => {
+    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
+      verbose: true, // https://labs.convex.dev/auth/debugging
+    })
+    const convexQueryClient = new ConvexQueryClient(convex)
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          queryKeyHashFn: convexQueryClient.hashFn(),
+          queryFn: convexQueryClient.queryFn(),
+        },
+      },
+    })
+    convexQueryClient.connect(queryClient)
+
+    return { convex, queryClient }
+  }, [])
+
   return (
     <ConvexAuthNextjsProvider client={convex}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
