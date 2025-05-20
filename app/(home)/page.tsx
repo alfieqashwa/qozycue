98fa54fa8f2d9fd06c1844e4ccb9855ff528ebc1
Alfie Qashwa <alfieqashwa@gmail.com>
2025-05-20 20:15:23 +0700

    feat: add marketing button on Home Page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { HoverBorderGradient } from "@/components/hover-border-gradient"
 import { ShimmerButton } from "@/components/shimmer-button"
 import { Vortex } from "@/components/vortex"
 import { api } from "@/convex/_generated/api"
@@ -18,12 +19,19 @@ export default async function HomePage() {
 
   return (
     <Vortex rangeY={400} className="z-50">
+      <section className="flex justify-center text-center">
+        <HoverBorderGradient>
+          <span className="animate-pulse px-0 text-xs font-semibold text-fuchsia-500 uppercase lg:px-2 lg:text-sm">
+            free if you only have 10 pool tables
+          </span>
+        </HoverBorderGradient>
+      </section>
       <Hero />
       <section className="flex w-full items-center justify-between px-6 py-6 sm:hidden">
         <GiPoolTriangle className="animate-pulse-slow size-14 text-fuchsia-600 sm:size-16 lg:size-24" />
         <Utensils className="animate-pulse-slow size-12 text-fuchsia-600 sm:size-14 lg:size-[5rem]" />
       </section>
-      <div className="flex flex-col items-center justify-center gap-4 sm:mt-8 sm:flex-row md:gap-8 lg:gap-16">
+      <section className="flex flex-col items-center justify-center gap-4 sm:mt-8 sm:flex-row md:gap-8 lg:gap-16">
         <DocumentationButton
           icon={
             <GiPoolTriangle
@@ -35,19 +43,20 @@ export default async function HomePage() {
         />
         <ShimmerButton>
           <a
-            href="https://www.youtube.com/playlist?list=PLnWWQuvB-wxg2PHPh4aybEZaqREqcrVuN"
+            // href="https://www.youtube.com/playlist?list=PLnWWQuvB-wxg2PHPh4aybEZaqREqcrVuN"
+            href="https://docs.qozycue.com"
             target="_blank"
             rel="noopener noreferrer"
             className="flex justify-center text-center"
           >
             <Tv className="mr-1.5 animate-pulse text-fuchsia-500" />
-            Tutorial
+            Tutorial (WIP)
           </a>
         </ShimmerButton>
-      </div>
-      <div className="flex items-center justify-center gap-4">
+      </section>
+      <section className="flex items-center justify-center gap-4">
         <AuthButtons slug={slug} />
-      </div>
+      </section>
     </Vortex>
   )
 }
