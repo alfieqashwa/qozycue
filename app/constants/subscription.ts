6aba3d0a9d10d7f3b28712397e3d1e5419e3f68e
Alfie Qashwa <alfieqashwa@gmail.com>
2025-06-25 19:38:10 +0700

    feat: add subscription dialog info

---
+++ app/constants/subscription.ts
@@ -0,0 +1,5 @@
+export const SUBSCRIPTION = [
+  { type: "TRIAL", poolTables: 10, products: 20, packets: 6, teams: 5 },
+  { type: "BASIC", poolTables: 20, products: 60, packets: 20, teams: 7 },
+  { type: "PRO", poolTables: 10, products: 100, packets: 20, teams: 10 },
+]
